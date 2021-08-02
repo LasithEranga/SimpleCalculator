3b8ddc70ee55f2b13f309e02faf6f94ad04c1186
LasithEranga <lasitheranga1@gmail.com>
2021-08-02 23:37:41 +0530

    Functions

--- Simple Calculator/Simple Calculator/MainPage.xaml.cs
+++ Simple Calculator/Simple Calculator/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using org.mariuszgromada.math.mxparser;
 
 namespace Simple_Calculator
 {
@@ -16,11 +17,111 @@ namespace Simple_Calculator
         }
         
 
-        int count = 0;
-        void Handle_Clicked(object sender, System.EventArgs e)
+        int sum = 0;
+        void click1(object sender, System.EventArgs e)
         {
-            count++;
-            ((Button)sender).Text = $"You clicked {count} times.";
+            txt1.Text = txt1.Text+"1";
+            
+        }
+
+        void click2(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "2";
+
+        }
+
+        void click3(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "3";
+
+        }
+
+        void click4(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "4";
+
+        }
+
+        void click5(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "5";
+
+        }
+
+        void click6(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "6";
+
+        }
+
+        void click7(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "7";
+
+        }
+
+        void click8(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "8";
+
+        }
+
+        void click9(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "9";
+
+        }
+
+        void click0(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "0";
+
+        }
+
+        void plus(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "+";
+
+        }
+
+        void minus(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "-";
+
+        }
+
+        void divide(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "/";
+
+        }
+
+        void multiply(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + "*";
+
+        }
+
+        void docalc(object sender, System.EventArgs e)
+        {
+            Expression ex = new Expression(txt1.Text);
+            double v = ex.calculate();
+            txt1.Text = v.ToString();
+
+        }
+
+        void dot(object sender, System.EventArgs e)
+        {
+            txt1.Text = txt1.Text + ".";
+
+        }
+
+
+
+        void clear(object sender, System.EventArgs e)
+        {
+            txt1.Text = "";
+
         }
     }
 
